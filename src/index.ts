256097b542b26112ba47dd9e8ea9623010b5a404
SarawutCoding <sarawutphuangsumalee@gmail.com>
2026-06-22 21:13:39 +0700

    save

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
 
 new Elysia()
-  .get("/", () => "Hello Elysia")
-  .get("/Hello", () => {
-    return "Hello World";
-  })
-  .listen(3000);
-
+    .get('/user/:id', ({ params: { id } }) => id)
+    .listen(3000)
+    
 console.log(
   `🦊 Elysia is running`
 );
